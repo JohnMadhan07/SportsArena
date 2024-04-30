0ff8b3d5415dffa86aacc65e074dc5e91f77d92f
Madhan <madhan12n@gmail.com>
2024-04-30 10:50:38 +0100

    Navigation to Games menu when book button is clicked

--- app/src/main/java/ie/setu/sportsarena/ui/home/HomeFragment.kt
+++ app/src/main/java/ie/setu/sportsarena/ui/home/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import ie.setu.sportsarena.R
 import ie.setu.sportsarena.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -33,6 +36,18 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Find the "Book" button
+        val bookButton: Button = binding.buttonBook
+
+        // Set click listener for the "Book" button
+        bookButton.setOnClickListener {
+            val navController = findNavController()
+            navController.navigate(R.id.action_homeFragment_to_gamesFragment)
+        }
     }
 
     override fun onDestroyView() {
