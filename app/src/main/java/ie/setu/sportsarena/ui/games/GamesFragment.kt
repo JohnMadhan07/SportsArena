9819e9d194b08b8c7c94a160d248d87df7ef43b9
Madhan <madhan12n@gmail.com>
2024-05-01 09:59:01 +0100

    Configured the RecyclerView in the GamesFragment layout to display the list of games

--- app/src/main/java/ie/setu/sportsarena/ui/games/GamesFragment.kt
+++ app/src/main/java/ie/setu/sportsarena/ui/games/GamesFragment.kt
@@ -1,42 +1,41 @@
 package ie.setu.sportsarena.ui.games
-
+import GameAdapter
+import GamesViewModel
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import ie.setu.sportsarena.databinding.FragmentGamesBinding
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ie.setu.sportsarena.R
 
 class GamesFragment : Fragment() {
 
-    private var _binding: FragmentGamesBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var gameAdapter: GameAdapter
+    private val viewModel: GamesViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(GamesViewModel::class.java)
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_games, container, false)
 
-        _binding = FragmentGamesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        // Initialize RecyclerView
+        recyclerView = root.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        val textView: TextView = binding.textGames
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // Initialize GameAdapter
+        gameAdapter = GameAdapter()
+        recyclerView.adapter = gameAdapter
+
+        // Observe gamesList LiveData and update the adapter when the list changes
+        viewModel.gamesList.observe(viewLifecycleOwner) { games ->
+            gameAdapter.submitList(games)
         }
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return root
     }
 }
