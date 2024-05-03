42dd64a38af6525684ebc1d2a7632f4378380971
Madhan <madhan12n@gmail.com>
2024-05-03 14:03:02 +0100

    Added the Architecture logic for the TimeSlot Fragment

---
+++ app/src/main/java/ie/setu/sportsarena/ui/timeslot/TimeSlotFragment.kt
@@ -0,0 +1,53 @@
+package ie.setu.sportsarena.ui.timeslot
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ie.setu.sportsarena.R
+import ie.setu.sportsarena.adapters.TimeSlotAdapter
+
+class TimeSlotFragment : Fragment() {
+
+    private lateinit var timeSlotViewModel: TimeSlotViewModel
+    private lateinit var timeSlotAdapter: TimeSlotAdapter
+    private lateinit var timeSlotsRecyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_timeslot, container, false)
+        timeSlotsRecyclerView = view.findViewById(R.id.timeSlotsRecyclerView)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        // Initialize ViewModel
+        timeSlotViewModel = ViewModelProvider(this).get(TimeSlotViewModel::class.java)
+
+        // Initialize Adapter with an empty list initially
+        timeSlotAdapter = TimeSlotAdapter(emptyList())
+
+        // Set up RecyclerView
+        timeSlotsRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        timeSlotsRecyclerView.adapter = timeSlotAdapter
+
+        // Observe time slots from ViewModel
+        timeSlotViewModel.timeSlots.observe(viewLifecycleOwner) { timeSlots ->
+            // Update RecyclerView with time slots
+            timeSlotAdapter.submitList(timeSlots)
+        }
+    }
+}
+
---
+++ app/src/main/java/ie/setu/sportsarena/ui/timeslot/TimeSlotViewModel.kt
@@ -0,0 +1,32 @@
+package ie.setu.sportsarena.ui.timeslot
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ie.setu.sportsarena.models.TimeSlot
+
+
+class TimeSlotViewModel : ViewModel() {
+    private val _timeSlots = MutableLiveData<List<TimeSlot>>()
+
+    // Initialize some example time slots
+    init {
+        val exampleTimeSlots = listOf(
+            TimeSlot("9:00 AM", "10:00 AM"),
+            TimeSlot("10:00 AM", "11:00 AM"),
+            TimeSlot("11:00 AM", "12:00 PM"),
+            TimeSlot("12:00 AM", "13:00 PM"),
+            TimeSlot("13:00 AM", "14:00 PM"),
+            TimeSlot("14:00 AM", "15:00 PM"),
+            TimeSlot("15:00 AM", "16:00 PM"),
+            TimeSlot("16:00 AM", "17:00 PM"),
+
+            // Add more time slots as needed
+        )
+        _timeSlots.value = exampleTimeSlots
+    }
+
+    // Expose LiveData to observe time slots
+    val timeSlots: LiveData<List<TimeSlot>>
+        get() = _timeSlots
+}
