38fa59638421df5137b41a600d4478c301da7e2f
Madhan <madhan12n@gmail.com>
2024-05-04 20:59:35 +0100

    Modified TimeSlot ViewModel to create a timeslot in DB

--- app/src/main/java/ie/setu/sportsarena/ui/timeslot/TimeSlotViewModel.kt
+++ app/src/main/java/ie/setu/sportsarena/ui/timeslot/TimeSlotViewModel.kt
@@ -3,28 +3,33 @@ package ie.setu.sportsarena.ui.timeslot
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ie.setu.sportsarena.firebase.FirebaseDBManager
 import ie.setu.sportsarena.models.TimeSlot
-
+import timber.log.Timber
 
 class TimeSlotViewModel : ViewModel() {
-    private val _timeSlots = MutableLiveData<List<TimeSlot>>()
+    private val _timeSlots: MutableLiveData<List<TimeSlot>> = MutableLiveData()
+    val timeSlots: LiveData<List<TimeSlot>> = _timeSlots
 
-    // Initialize some example time slots
     init {
-        val exampleTimeSlots = listOf(
-            TimeSlot("9:00 AM", "10:00 AM"),
-            TimeSlot("10:00 AM", "11:00 AM"),
-            TimeSlot("11:00 AM", "12:00 PM"),
-            TimeSlot("12:00 AM", "13:00 PM"),
-            TimeSlot("13:00 AM", "14:00 PM"),
-            TimeSlot("14:00 AM", "15:00 PM"),
-            TimeSlot("15:00 AM", "16:00 PM"),
-            TimeSlot("16:00 AM", "17:00 PM")
-        )
-        _timeSlots.value = exampleTimeSlots
+        // Initialize timeSlots LiveData with an empty list
+        _timeSlots.value = emptyList()
     }
 
-    // Expose LiveData to observe time slots
-    val timeSlots: LiveData<List<TimeSlot>>
-        get() = _timeSlots
+    fun addTimeSlot(timeSlot: TimeSlot) {
+        Timber.d("Before calling FirebaseDBManager.create(timeSlot)")
+        try {
+            FirebaseDBManager.create(timeSlot)
+            // Add the new time slot to the existing list and update the LiveData
+            val currentList = _timeSlots.value.orEmpty().toMutableList()
+            currentList.add(timeSlot)
+            _timeSlots.value = currentList
+        } catch (e: IllegalArgumentException) {
+            Timber.e(e, "Error adding time slot")
+        }
+        Timber.d("After calling FirebaseDBManager.create(timeSlot)")
+    }
 }
+
+
+
