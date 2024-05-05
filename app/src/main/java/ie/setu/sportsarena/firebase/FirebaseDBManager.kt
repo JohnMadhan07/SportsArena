0229df5b1762be2eb3994d1d0638b7cfeae5c85a
Madhan <madhan12n@gmail.com>
2024-05-05 11:14:31 +0100

    Created function to retrieve data from Db

--- app/src/main/java/ie/setu/sportsarena/firebase/FirebaseDBManager.kt
+++ app/src/main/java/ie/setu/sportsarena/firebase/FirebaseDBManager.kt
@@ -2,8 +2,11 @@ package ie.setu.sportsarena.firebase
 
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import ie.setu.sportsarena.models.TimeSlot
 import ie.setu.sportsarena.models.TimeSlotStore
 import timber.log.Timber
@@ -11,17 +14,38 @@ import timber.log.Timber
 object FirebaseDBManager : TimeSlotStore {
     var database: DatabaseReference = FirebaseDatabase.getInstance().reference
     override fun findAll(timeSlotsList: MutableLiveData<List<TimeSlot>>) {
-        // Implement the logic to retrieve all time slots from Firebase and update the MutableLiveData object
-        // For example:
-        // val allTimeSlots = // Logic to fetch time slots from Firebase
-        // timeSlotsList.postValue(allTimeSlots)
+        // Assuming you have a FirebaseUser available somewhere in your app
+        val uid = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+
+        // Reference to the "user-timeSlots" node for the current user
+        val userTimeSlotsRef = database.child("user-timeSlots").child(uid)
+
+        // Add a listener to fetch time slots for the current user
+        userTimeSlotsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val localList = mutableListOf<TimeSlot>()
+
+                // Iterate through the dataSnapshot to retrieve TimeSlot objects
+                snapshot.children.forEach { timeSlotSnapshot ->
+                    val timeSlot = timeSlotSnapshot.getValue(TimeSlot::class.java)
+                    timeSlot?.let {
+                        localList.add(it)
+                    }
+                }
+
+                // Update the MutableLiveData with the fetched time slots
+                timeSlotsList.value = localList
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Timber.i("Firebase TimeSlot error : ${error.message}")
+            }
+        })
     }
 
+
     override fun findById(timeSlotId: String, timeSlot: MutableLiveData<TimeSlot>) {
-        // Implement the logic to find a time slot by its ID from Firebase and update the MutableLiveData object
-        // For example:
-        // val foundTimeSlot = // Logic to fetch time slot from Firebase by ID
-        // timeSlot.postValue(foundTimeSlot)
+
     }
 
     override fun create(timeSlot: TimeSlot) {
@@ -51,10 +75,10 @@ object FirebaseDBManager : TimeSlotStore {
 
 
     override fun delete(timeSlotId: String) {
-        // Implement the logic to delete a time slot from Firebase by its ID
+
     }
 
     override fun update(timeSlotId: String, timeSlot: TimeSlot) {
-        // Implement the logic to update a time slot in Firebase by its ID
+
     }
 }
